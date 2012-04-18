7e178239a7e2d8156d7ce1e7bccf0e052c220114
Trevor Jones <tdjones@ualberta.ca>
2012-04-18 16:01:45 -0600

    Moved User model association into dispatcher and added fix for development mode. Now can be run in dev environment.

--- init.rb
+++ init.rb
@@ -1,13 +1,15 @@
 require 'redmine'
 require 'dispatcher'
-require_dependency 'principal'
-require_dependency 'user'
 
 require_dependency 'gitosis'
 require_dependency 'gitosis_job' #Needed for delayed_job
 require_dependency 'gitosis/patches/repositories_controller_patch'
 require_dependency 'gitosis/patches/repositories_helper_patch'
 require_dependency 'gitosis/patches/git_adapter_patch'
+
+if RAILS_ENV == 'development'
+  ActiveSupport::Dependencies.load_once_paths.reject!{|x| x =~ /^#{Regexp.escape(File.dirname(__FILE__))}/}
+end
 
 Redmine::Plugin.register :redmine_gitosis do
   name 'Redmine Gitosis plugin'
@@ -22,6 +24,7 @@ Redmine::Plugin.register :redmine_gitosis do
     'basePath' => '/srv/projects/git/repositories/',
     }, 
     :partial => 'redmine_gitosis'
+  menu :account_menu, :mysshkeys, { :controller => 'gitosis_public_keys', :action => 'index' }, {:caption => 'My SSH Keys', :if => Proc.new { User.current.logged? }, :before => :my_account}
 end
 
 # initialize hook
@@ -32,9 +35,6 @@ end
 class GitosisProjectShowHook < Redmine::Hook::ViewListener
   render_on :view_projects_show_left, :partial => 'redmine_gitosis'
 end
-
-# initialize association from user -> public keys
-User.send(:has_many, :gitosis_public_keys, :dependent => :destroy)
 
 Dispatcher.to_prepare :redmine_gitosis do
   # apply GitAdapter patch
@@ -53,4 +53,9 @@ Dispatcher.to_prepare :redmine_gitosis do
   unless ActiveRecord::Base.observers.include?(GitosisObserver) || File.basename($0) == 'rake'
     ActiveRecord::Base.observers = ActiveRecord::Base.observers << GitosisObserver
   end
+  require_dependency 'principal'
+  require_dependency 'user'
+  # initialize association from user -> public keys
+  User.send(:has_many, :gitosis_public_keys, :dependent => :destroy)
+
 end
